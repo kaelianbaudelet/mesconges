6e1e9dd2a9c1282c1d5d289532d7f08f2de0f65b
Kaelian <aconique@gmail.com>
2024-11-28 22:45:43 +0100

    v3.2

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -7,8 +7,7 @@ import { logout } from "@/app/actions/logout";
 const LogoutPage = () => {
   useEffect(() => {
     logout();
-    window.location.href =
-      "https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=http://localhost:3000/";
+    window.location.href = `https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=${process.env.SITE_URL}`;
   }, []);
 
   return null;
