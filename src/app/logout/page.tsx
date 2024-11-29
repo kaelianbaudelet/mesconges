8fa8b88fb8efee50c8a0fb787399f0797e70ed96
Kaelian <aconique@gmail.com>
2024-11-29 22:56:38 +0100

    d

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -8,6 +8,8 @@ const LogoutPage = () => {
     logout();
 
     window.location.href = `https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=${window.location.origin}`;
+
+    alert("Vous avez été déconnecté.");
   }, []);
 
   return null;
