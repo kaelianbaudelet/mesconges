952429fc83630b4a816fc11799bc22f6468f3e85
Kaelian <aconique@gmail.com>
2024-11-29 22:54:28 +0100

    d

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -6,10 +6,8 @@ import { logout } from "@/app/actions/logout";
 const LogoutPage = () => {
   useEffect(() => {
     logout();
-    setTimeout(() => {
-      window.location.href = `https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=${window.location.origin}`;
-      window.close();
-    }, 2000);
+
+    window.location.href = `https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=${window.location.origin}`;
   }, []);
 
   return null;
