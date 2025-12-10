f2a99fb94fff058aa17337d6bd842b270add4d83
Kaelian Baudelet <aconique@gmail.com>
2025-12-10 20:16:36 +0100

    update to next 16.0.8 - fix CVE React2Shell

--- next.config.ts
+++ next.config.ts
@@ -16,7 +16,12 @@ const cspHeader = `
 const nextConfig: NextConfig = {
   output: "standalone",
   images: {
-    domains: ["lh3.googleusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
   },
   async headers() {
     return [
